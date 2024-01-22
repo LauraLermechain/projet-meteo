91706a4419b12fee6b7b0daaea8083f050ea09f6
Nicolas Descarpentries <nicolas.descarp@outlook.fr>
2024-01-22 17:11:07 +0100

    correction parenthèses DB

--- baseDeDonnee.py
+++ baseDeDonnee.py
@@ -15,13 +15,13 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS Releve (
                pression_releve FLOAT NOT NULL,
                date_time_releve DATETIME NOT NULL,
                id_sonde INTEGER,
-               FOREIGN KEY (id_sonde) REFERENCES Genres(id_sonde)''')
+               FOREIGN KEY (id_sonde) REFERENCES Sonde(id_sonde))''')
 connection.commit()
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS utilisateur (
                id_utilisateur INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                nom_utilisateur VARCHAR(50) NOT NULL,
-               mot_de_passe_utilisateur VARCHAR(50) NOT NULL''')
+               mot_de_passe_utilisateur VARCHAR(50) NOT NULL)''')
 connection.commit()
 
 connection.close()
